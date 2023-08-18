0409cc95940aa75629eb2b4dddab98b119cc37e8
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-08-18 22:18:06 +0700

    remove bg-primary from layout, use divider instead

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,11 @@
 </script>
 
 <div class="flex min-h-screen gap-4 p-12">
-	<div class="w-1/2 md:w-1/4 bg-secondary">
+	<div class="w-1/2 md:w-1/4">
 		<Sidebar />
 	</div>
-	<div class="bg-secondary">
+	<div class="divider divider-horizontal" />
+	<div class="">
 		<slot />
 	</div>
 </div>
