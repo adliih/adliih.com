378ff6bf81b0d297f40fc4d8fede2b6238dc0f6c
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-08-18 22:02:07 +0700

    layouting + sidebar

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,60 @@
+<script>
+	// @ts-ignore
+	import Icon from 'fa-svelte';
+	import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+	import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
+
+	const pages = [
+		{
+			href: '/about',
+			title: 'About'
+		},
+		{
+			href: '/portfolio',
+			title: 'Portfolio'
+		}
+	];
+
+	const socials = [
+		{
+			icon: faLinkedin,
+			href: 'https://www.linkedin.com/in/adliih/'
+		},
+		{
+			icon: faGithub,
+			href: 'https://github.com/adliih'
+		},
+		{
+			icon: faEnvelopeOpenText,
+			href: 'mailto:mail@adliih.com'
+		}
+	];
+</script>
+
+<div class="flex flex-col items-center justify-center h-full gap-16 bg-secondary">
+	<div class="avatar">
+		<div class="w-24 rounded">
+			<img src="/placeholder.svg" alt="Profile Picture" class="w-48 h-48 mb-4 rounded-full" />
+		</div>
+	</div>
+
+	<nav class="w-full">
+		<ul class="menu menu-vertical menu-lg rounded-box">
+			{#each pages as page}
+				<li>
+					<a href={page.href} class="block py-2 text-center hover:text-accent">{page.title}</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+
+	<ul class="menu menu-horizontal">
+		{#each socials as social}
+			<li>
+				<a href={social.href} class="hover:text-accent">
+					<Icon icon={social.icon} />
+				</a>
+			</li>
+		{/each}
+	</ul>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
 	import '../app.css';
+	import Sidebar from '../components/Sidebar.svelte';
 </script>
 
-<slot />
+<div class="flex min-h-screen p-12">
+	<div class="w-1/2 md:w-1/4">
+		<Sidebar />
+	</div>
+	<div class="p-4">
+		<slot />
+	</div>
+</div>
