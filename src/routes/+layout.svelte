3d3885b80a8b63c39527a1608ea69f1e96199c66
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-08-19 01:12:21 +0700

    sticky sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,12 @@
 	import Sidebar from '$components/Sidebar.svelte';
 </script>
 
-<div class="flex min-h-screen gap-4 p-12">
-	<div class="w-1/2 md:w-1/4">
+<div class="flex min-h-screen gap-4">
+	<div class="sticky top-0 w-1/2 h-screen p-4 flex-0 md:w-1/4">
 		<Sidebar />
 	</div>
-	<div class="divider divider-horizontal" />
-	<div class="">
+	<div class="py-4 -mx-4 divider divider-horizontal" />
+	<div class="flex-1 px-4 py-12 overflow-y-auto">
 		<slot />
 	</div>
 </div>
