94a8a2af917ebfece779c872d17662a9d61fee2a
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-08-28 10:59:55 +0700

    add hygraph secrets

--- src/lib/server/secrets.ts
+++ src/lib/server/secrets.ts
@@ -1,5 +1,8 @@
-import { USER_ID } from '$env/static/private';
+import { USER_ID, HYGRAPH_GRAPHQL_URL } from '$env/static/private';
 
 export const secrets = {
-	userId: USER_ID
+	userId: USER_ID,
+	hygraph: {
+		url: HYGRAPH_GRAPHQL_URL
+	}
 };
