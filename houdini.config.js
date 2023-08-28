1be18b2ef3e3d002fcbd3c7d64218b397406ca3d
Adli Ihsan Hariadi <11794203+adliih@users.noreply.github.com>
2023-08-28 18:03:59 +0700

    optional process on houdini config

--- houdini.config.js
+++ houdini.config.js
@@ -2,7 +2,7 @@
 
 /** @type {import('houdini').ConfigFile} */
 const config = {
-	watchSchema: { url: process.env.HYGRAPH_GRAPHQL_URL, interval: 0 },
+	watchSchema: { url: process?.env?.HYGRAPH_GRAPHQL_URL, interval: 0 },
 	plugins: {
 		'houdini-svelte': {}
 	},
